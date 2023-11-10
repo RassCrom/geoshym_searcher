54dbeeff7f5bc8c1d53065da9d900f307de17499
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2023-11-10 11:26:07 +0600

    Descriptive comments

--- pgdb.js
+++ pgdb.js
@@ -17,24 +17,44 @@ const db = pgp({
 
 app.use(cors());
 
+/**
+ * Generates a case-insensitive regular expression pattern for a given search term.
+ *
+ * @param {string} searchTerm - The input search term for which the regex pattern is generated.
+ * @returns {RegExp} - The generated regular expression pattern.
+ */
 function generateRegex(searchTerm) {
+    // Escape special characters in the search term to ensure they are treated as literals in the regex pattern
     const escapedSearchTerm = searchTerm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    const regexPattern = new RegExp(`${escapedSearchTerm}`, 'i');
+    const regexPattern = new RegExp(`${escapedSearchTerm}`, 'i'); // Create a case-insensitive regex
     
     return regexPattern;
 };
 
+/**
+ * Converts Well-Known Binary (WKB) geometry to coordinates using the wkx library and Turf.js.
+ *
+ * @param {string} wkb - Well-Known Binary representation of a geometry (hex-encoded).
+ * @returns {number[]} - The coordinates of the centroid of the geometry.
+ */
 function convertWkbToCoords(wkb) {
     const geometry = wkx.Geometry.parse(Buffer.from(wkb, 'hex'));
     const coordinates = geometry.toGeoJSON();
-    const t = turf.centroid(coordinates);
+    const centroid = turf.centroid(coordinates);
 
-    return t.geometry.coordinates
+    return centroid.geometry.coordinates
 }
 
-function relevantSuggestions(resData, userInput) {
+/**
+ * Filters and transforms a list of suggestions based on user input.
+ *
+ * @param {Array} resData - An array of suggestion data objects.
+ * @param {string} userInput - The user's input used for filtering suggestions.
+ * @returns {Array} - An array of relevant suggestions containing name and coordinates.
+ */
+function relevantSuggestions(dbData, userInput) {
     const userRegex = generateRegex(userInput)
-    const matchedSuggestion = resData
+    const matchedSuggestion = dbData
         .filter((el) => userRegex.test(el.name))
         .map((el) => ({ name: el.name, geom: convertWkbToCoords(el.geom) }))
 
@@ -46,6 +66,7 @@ app.get('/api/data', async (req, res) => {
         const userInput = req.query.query; // Get the query parameter from the client
         const query = `select name, geom from buildings;`;
 
+        // Get response from database
         const data = await db.any(query);
 
         const suggestions = relevantSuggestions(data, userInput);
